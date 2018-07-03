fac97427198fb40ccce109fcff0da24ef110037b
sooylee <syunws@gmail.com>
2018-07-03 15:56:28 +0900

    Component 생성 예제 - 강의 목표 출력하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const goals = [
+  '1. React 개발에 필요한 환경을 구축한다.',
+  '2. 새로운 자바스트림트 문법을 익힌다',
+  '3. 개발 편의를 위한 VSCode IDE를 익힌다',
+];
+
+const LectureGoalList = props => {
+  console.log(props);
+  return (
+    <div>
+      <div>{props.title}</div>
+      <ul>
+        {props.items.map((item, idx) => <li key={idx}>{item}</li>)}
+        {props.items.map(item => <li key={item}>{item}</li>)}
+      </ul>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
@@ -13,6 +31,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <LectureGoalList items={goals} title={'강의 목표!'} />
       </div>
     );
   }
---
+++ src/es6.js
@@ -0,0 +1,13 @@
+function f()
+{
+  return 1;
+}
+
+f();  //1
+
+
+const f = () =>{
+  return 1;
+}
+
+const k = () => <div>XXXX</div>;
