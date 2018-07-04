7727453a33b70b0116d1481b339f8b7c05656828
sooylee <syunws@gmail.com>
2018-07-04 10:47:53 +0900

    React Component 만들기 예제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
+import LectureGoalList from './components/LectureGoalList';
 import logo from './logo.svg';
 import './App.css';
 const goals = [
-  '1. React 개발에 필요한 환경을 구축한다.',
-  '2. 새로운 자바스트림트 문법을 익힌다',
-  '3. 개발 편의를 위한 VSCode IDE를 익힌다',
+  'React 개발에 필요한 환경을 구축한다.',
+  '새로운 자바스트림트 문법을 익힌다',
+  '개발 편의를 위한 VSCode IDE를 익힌다',
 ];
 
-const LectureGoalList = props => {
-  console.log(props);
-  return (
-    <div>
-      <div>{props.title}</div>
-      <ul>
-        {props.items.map((item, idx) => <li key={idx}>{item}</li>)}
-        {props.items.map(item => <li key={item}>{item}</li>)}
-      </ul>
-    </div>
-  );
-};
 
 class App extends Component {
   render() {
