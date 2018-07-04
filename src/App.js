5c01d8ee8b1e0efaf6a729ab75bfbb2f47f0f2e3
sooylee <syunws@gmail.com>
2018-07-04 16:11:42 +0900

    Component callback 구현 예제, shouldComponentUpdate, componentWillUnmount 사용하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LectureGoalList from './components/LectureGoalList';
 import Timer from './components/Timer';
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 const goals = [
   'React 개발에 필요한 환경을 구축한다.',
   '새로운 자바스트림트 문법을 익힌다',
@@ -10,11 +11,25 @@ const goals = [
 ];
 
 class App extends Component {
+  state = {
+    isExpired: false,
+  };
   handleClick = e => {
     //debugger;
     console.log(this, e);
   };
+
+  handleComplete = () => {
+    //자식이 종료되고 직후 1초후에
+    console.log('handleComplete!');
+
+    this.setState({ isExpired: true });
+  };
   render() {
+    //const isExpired = moment('2018-07-04T17:00:00+0900') < moment();
+
+    const { isExpired } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -26,7 +41,8 @@ class App extends Component {
         </p>
 
         <LectureGoalList items={goals} title={'강의 목표!'} />
-        <Timer expireDate="2018-07-04T17:00:00+0900" />
+
+        {!isExpired && <Timer expireDate="2018-07-04T16:10:00+0900" onComplete={this.handleComplete} />}
       </div>
     );
   }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,29 +4,65 @@ import 'moment/locale/ko';
 moment.local = 'ko';
 //moment.relativeTimeThreshold('m', 1440); // 24시간 이내는 분으로 나타내도록
 
+const TIME_FORMAT = 'YYYYMMDD hh:mm'; //file scope 을 가질 수 있도록 wrapping 함
+
 class Timer extends Component {
   constructor(props) {
+    //console.log(this.props); // this 없음
     super(props);
 
-    // this.state = {
-    //   date: moment(),
-    // };
+    console.log(this.props); // this 생성됨
+    this.state = {
+      date: moment(),
+    };
 
-    this.nTimer = setInterval(this.handleTick, 1000);
+    this.nTimer = setInterval(() => {
+      this.setState({
+        date: moment(),
+      }); // forceUpdate 하지 않더라도 무조건 다시 Rendering을 함
+    }, 1000);
   }
 
+  /*
   handleTick = () => {
     console.log(1);
     this.forceUpdate();
   };
+  */
+
+  shouldComponentUpdate(nextprops, nextState) {
+    if (moment(this.props.expireDate) > this.state.date) {
+      const prevDateStr = this.state.date.format(TIME_FORMAT);
+      const nextDateStr = nextState.date.format(TIME_FORMAT);
+
+      return prevDateStr < nextDateStr;
+    } else return false;
+  }
 
   render() {
+    console.log('render!');
+    const { expireDate, onComplete } = this.props;
+    const { date } = this.state;
+    if (moment(expireDate) < date) {
+      setTimeout(() => {
+        onComplete && onComplete();
+      }, 1000);
+      return <div>종료되었습니다.</div>;
+    }
+
     return (
       <div>
-        <div>현재시간은 :{moment().format('YYYY-MM-DD A hh:mm:ss')} </div>
-        <div>{moment(this.props.expireDate).fromNow()} 에 종료 합니다. </div>
+        <div>현재시간은 :{moment().format(TIME_FORMAT)} </div>
+        <div>{moment(expireDate).fromNow()} 에 종료 합니다. </div>
       </div>
     );
+  }
+
+  componentWillUnmount() {
+    if (this.nTimer) {
+      clearInterval(this.nTimer);
+      this.nTimer = null;
+    }
   }
 }
 
